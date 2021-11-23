16f37e1267a84fd1fb50a6c5564568dbfef13e99
dtlucho <luciano.ditomaso@gmail.com>
2021-11-23 17:54:46 -0300

    Circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://as.com/meristation/imagenes/2020/12/17/betech/1608203224_632911_1608203373_noticia_normal_recorte1.jpg',
+              ),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          image: NetworkImage(
+            'https://as.com/meristation/imagenes/2020/12/17/betech/1608203224_632911_1608203373_noticia_normal_recorte1.jpg',
+          ),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
