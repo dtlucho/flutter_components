bb58d5c5ee0c32eb15907abfc0477a38b52c5c91
dtlucho <luciano.ditomaso@gmail.com>
2021-11-23 17:45:17 -0300

    Alert dialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,12 +9,54 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showAlert(context),
+          child: const Text('Mostrar alerta'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: const StadiumBorder(),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.face),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: const Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('This is the content for the alert dialog'),
+              FlutterLogo(size: 100.0),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
