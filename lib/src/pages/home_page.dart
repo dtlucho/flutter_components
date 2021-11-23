84532d9126b3a373df3ad4065a1e852a0ced2863
dtlucho <luciano.ditomaso@gmail.com>
2021-11-23 13:15:15 -0300

    Navigation

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_components/src/providers/menu_provider.dart';
 import 'package:flutter_components/src/utils/icon_string_util.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,13 +24,13 @@ class HomePage extends StatelessWidget {
       initialData: const [],
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listItems(snapshot.data),
+          children: _listItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listItems(List<dynamic>? data) {
+  List<Widget> _listItems(List<dynamic>? data, BuildContext context) {
     final List<Widget> options = [];
 
     for (var option in data!) {
@@ -37,7 +38,12 @@ class HomePage extends StatelessWidget {
         leading: getIcon(option['icon']),
         title: Text(option['text']),
         trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context) => const AlertPage(),
+          );
+          Navigator.push(context, route);
+        },
       );
 
       options
