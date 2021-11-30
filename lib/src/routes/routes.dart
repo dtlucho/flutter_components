662eddb6a28315cfe968783c2d4174770e01541a
dtlucho <luciano.ditomaso@gmail.com>
2021-11-29 21:15:40 -0300

    Adding slider route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/card_page.dart';
 import 'package:flutter_components/src/pages/animated_container.dart';
 import 'package:flutter_components/src/pages/input_page.dart';
+import 'package:flutter_components/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() {
   return <String, WidgetBuilder>{
@@ -16,5 +17,6 @@ Map<String, WidgetBuilder> getAppRoutes() {
     'animatedContainer': (BuildContext context) =>
         const AnimatedContainerPage(),
     'inputs': (BuildContext context) => const InputPage(),
+    'slider': (BuildContext context) => const SliderPage(),
   };
 }
